db6437a9c5a6f890ab2a9ea58f14f4747e974eb0
Andreas Dewes <andreas.dewes@kiprotect.com>
2019-08-01 19:01:52 +0200

    v0.0.3

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@ from setuptools import find_packages
 
 setup(
     name="algoneer_datasets",
-    python_requires=">=3",
-    version="0.0.2",
+    python_requires=">=3.5",
+    version="0.0.3",
     author="Andreas Dewes",
     author_email="andreas.dewes@algoneer.org",
     license="Creative-Commons (license of datasets varies)",
     url="https://github.com/algoneer/datasets",
     packages=find_packages(),
     install_requires=["algoneer"],
+    package_data={'algoneer_datasets': ['**/*.gz', '**/*.yml']},
     zip_safe=False,
     entry_points={},
     description="Example datasets for Algoneer.",
